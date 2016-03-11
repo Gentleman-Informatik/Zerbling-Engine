67c2b27c71f8c85abdb90e5cb9f6886ad1e0219d
flaver12 <flaverkleiber@yahoo.de>
2016-03-11 17:57:14 +0100

    Added basic class for matrix

--- src/com/zerbling/math/Vector3f.java
+++ src/com/zerbling/math/Vector3f.java
@@ -1,3 +1,12 @@
+/**
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons,
+ * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ * 
+ * @author Flavio Kleiber <zerbarian@gmail.com>
+ * @desc Simple Vector3f class
+ */
+
 package com.zerbling.math;
 
 public class Vector3f {
